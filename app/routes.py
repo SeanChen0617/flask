cf1023512aa52aacd5ed6d98cd7458271e135ac9
Sean Chen <seanchen0617@gmail.com>
2019-11-01 17:51:11 +0800

    refactor structure

--- app/routes.py
+++ app/routes.py
@@ -1,69 +1,48 @@
 from flask import render_template, flash, redirect, url_for
 from app import app
-from app.forms import LoginForm, RegistrationForm
-from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+#from app.forms import LoginForm, RegistrationForm
+#from flask_login import current_user, login_user, logout_user, login_required
+from app.models import Stocks
+import twstock, time
+
 
 @app.route('/')
 @app.route('/index')
-@login_required
 def index():
-	"""
-	user = {'username': 'Sean'}
-	posts = [
-		{
-			'author': 'Chang',
-			'body': 'Chang in the house'
-		},
-		{
-			'author': 'Wang',
-			'body': 'Wang in the house'
-		}
+	return 'Hello there!'
+
+@app.route('/stock_monitor', methods=['GET', 'POST'])
+def stock_monitor():
+	result = []
+	monitored_stocks = [
+		{'stock_code': '2317', 'stock_name': '鴻海', 'stock_buy_in_price': 10.0},
+		{'stock_code': '0050', 'stock_name': '0050', 'stock_buy_in_price': 10.0},
+		{'stock_code': '2330', 'stock_name': 'TSMC', 'stock_buy_in_price': 10.0},
+		{'stock_code': '2454', 'stock_name': '聯發科', 'stock_buy_in_price': 10.0}
 	]
-	"""
-	return render_template('index.html', title='Home Page of Test', posts = posts)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-	if current_user.is_authenticated:
-		return redirect(url_for('index'))
-	form = LoginForm()
-	# form processing, in GET it returns false
-	if form.validate_on_submit():
-		user = User.query.filter_by(username=form.username.data).first()
-		if user is None or not user.check_password(form.password.data):
-			flash('Invalid user name or password')
-			return redirect(url_for('login'))
-		flash(user, remember_me=form.remember_me.data)
-		return redirect(url_for('index'))
-	return render_template('login.html', title='Sign In', form=form)
+	for _ in monitored_stocks:
+		try:
+			monitored_stocks_with_price = {}
+			# call twstock to get stock info
+			stock = twstock.realtime.get(_['stock_code'])
+			
+			monitored_stocks_with_price['stock_name'] = stock['info']['name']
+			monitored_stocks_with_price['stock_code'] = stock['info']['code']
+			monitored_stocks_with_price['stock_open_price'] = stock['realtime']['open']
+			monitored_stocks_with_price['stock_latest_price'] = stock['realtime']['latest_trade_price']
+			monitored_stocks_with_price['percentage'] = round((float(stock['realtime']['latest_trade_price']) - float(stock['realtime']['open']))/float(stock['realtime']['open'])*100, 3)
 
-@app.route('/logout')
-def logout():
-	logout_user()
-	return redirect(url_for('index'))
+			stock = twstock.Stock(_['stock_code'])
 
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-	if current_user.is_authenticated:
-		return redirect(url_for('index'))
-	form = RegistrationForm()
-	if form.validate_on_submit():
-		user = User(username=form.username.data, email=form.email.data)
-		user.set_password(form.password.data)
-		db.session.add(user)
-		db.session.commit()
-		flash('Register successfully. Welcome:{}'.format(form.username.data))
-		return redirect(url_for('login'))
-	flash('Test:{}'.format(form.username.data))
-	return render_template('register.html', title='Register', form=form)
+			monitored_stocks_with_price['stock_price'] = ', '.join(map(str,stock.high[-5:]))
+			monitored_stocks_with_price['stock_buy_in_price'] = _['stock_buy_in_price']
 
-@app.route('/user/<username>')
-@login_required
-def user(username):
-	user = User.query.filter_by(username=username).first_or_404()
-	posts = [
-		{'author': user, 'body': 'Test Post #1'},
-		{'author': user, 'body': 'Test Post #2'}
-	]
-	return render_template('user.html', user=user, posts=posts)
+			result.append(monitored_stocks_with_price)
+
+			time.sleep(3)
+		except TypeError:
+			print("latest_trade_price: %f", stock['realtime']['latest_trade_price'])
+			print("open: %f", stock['realtime']['open'])
+
+	return render_template('stock_monitor.html', stocks=result)
